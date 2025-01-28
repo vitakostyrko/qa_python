eedbe02450ac4107647f20dec98a8251dea2fca8
vitakostyrko <viktoria.kostyrko2@gmail.com>
2025-01-28 23:13:36 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -8,17 +8,69 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+from main import BooksCollector
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
+# обязательно указывать префикс Test
+class TestBooksCollector:
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
-    # напиши свои тесты ниже
+        # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_set_book_genre(self):
+        collector = BooksCollector()
+        collector.books_genre = {'Игра престолов': 'Фантастика',
+                                 'Еретик' : 'Ужасы',
+                                 'Каштановые человечки' : 'Детектив',
+                                 'Моана' : 'Мультфильм',
+                                 'Американский пирог' : 'Комедии'
+                                 }
+        collector.set_book_genre('Моана' , 'Мультфильм')
+        assert collector.books_genre ['Моана'] == 'Мультфильм'
+
+    def test_get_book_genre_good(self, name):
+        collector = BooksCollector()
+        collector.books_genre = {'Игра престолов': 'Фантастика',
+                                 'Еретик' : 'Ужасы',
+                                 'Каштановые человечки' : 'Детектив',
+                                 'Моана' : 'Мультфильм',
+                                 'Американский пирог' : 'Комедии'
+                                 }
+        assert collector.get_book_genre(name) == collector.books_genre[name]
+
+    def test_get_books_with_specific_genre_valid_genre_good(self):
+        collector = BooksCollector()
+        collector.books_genre = {'Игра престолов': 'Фантастика',
+                                 'Еретик' : 'Ужасы',
+                                 'Каштановые человечки' : 'Детектив',
+                                 'Моана' : 'Мультфильм',
+                                 'Американский пирог' : 'Комедии'
+                                 }
+        assert collector.get_books_with_specific_genre('Ужасы') == ['Еретик']
+
+    def get_books_for_children_no_added(self):
+        collector = BooksCollector()
+        collector.books_genre = {'Игра престолов': 'Фантастика',
+                                 'Еретик': 'Ужасы',
+                                 'Каштановые человечки': 'Детектив',
+                                 'Моана': 'Мультфильм',
+                                 'Американский пирог': 'Комедии'
+                                 }
+        assert collector.get_books_for_children() == ['Моана']
+
+    def add_book_in_favorites_my_again(self, name):
+        collector = BooksCollector()
+        collector.books_genre = {'Каштановые человечки': 'Детектив'}
+        collector.favorites = ['Каштановые человечки']
+        collector.add_book_in_favorites('Каштановые человечки')
+        assert collector.favorites == ['Каштановые человечки'] and len(collector.favorites) == 1
+
+    def test_delete_book_from_favorites_valid_name_true(self):
+        collector = BooksCollector()
+        collector.favorites = ['Моана', 'Игра престолов', 'Каштановые человечки']
+        collector.delete_book_from_favorites('Моана')
+        assert collector.favorites == ['Игра престолов', 'Каштановые человечки']
+
+    def test_get_list_of_favorites_books_true(self):
+        collector = BooksCollector()
+        collector.favorites = ['Моана', 'Игра престолов', 'Каштановые человечки']
+        assert collector.get_list_of_favorites_books() == collector.favorites
